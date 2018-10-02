5b339a0814dcbedf26d5444dd1128e73f8a0b285
siampako <kostas.siabanis@gmail.com>
2018-10-02 17:15:13 +0300

    Add withPrefix for logo image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "gatsby";
 import './_header.scss';
+import { withPrefix } from 'gatsby'
 
 const Header = ({isScrolled}) =>
   (
@@ -8,7 +9,7 @@ const Header = ({isScrolled}) =>
       <div className="container">
         <div className="row align-items-center justify-content-between d-flex">
           <div id="logo">
-            <a href="index.html"><img src="images/logo.png" alt="Startup"/></a>
+            <a href="index.html"><img src={withPrefix("images/logo.png")} alt="Startup"/></a>
           </div>
           <nav id="nav-menu-container">
             <ul className="nav-menu">
